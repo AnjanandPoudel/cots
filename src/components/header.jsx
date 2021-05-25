196f1b355a0787419184de1e7a34a78c0b46c975
Cruserap1 <cruserap@gmail.com>
2021-05-25 17:14:58 +0545

    2nd commit bg video

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,26 +4,33 @@ function Header(){
     return(
         <div className="header">
                 <header className="App-header ">
-                <div className="container navitems">
-                    <a href="/" className="white brandnav"> CoTs </a>
-                    <div className="navlinks">
-                        <a href="/" className="white nav"> Plans </a>
-                        <a href="/" className="white nav"> Blogs </a>
-                        <a href="/" className="white nav"> About </a>
-                        <i className="fa fa-search"></i>
-                    </div>
-                </div>
-                <div className="landing">
-                    <div className="landing-page">
-                        We strive for what we aspire
-                    </div>
-                    <div className="landing-page-image">
-                        <img className="landing-image" src="/images/image.png" alt="" />
-                    </div>
-                </div>
-                <i className="fa fa-arrow"></i>
+                    <video autoPlay muted loop className="videopart">
+                        <source src="/videos/vid.mp4" type="video/mp4" />
+                        Your browser does not support HTML5 video.
+                    </video>
+                            <div className="container navitems">
+                            <a href="/" className="white brandnav"> CoTs </a>
+                            <div className="navlinks">
+                                <a href="/" className="white nav"> Plans </a>
+                                <a href="/" className="white nav"> Blogs </a>
+                                <a href="/" className="white nav"> About </a>
+                                <i className="fa fa-search"></i>
+                            </div>
+                            </div>
+                            <div className="landing">
+                                <div className="landing-page">
+                                    We strive for what we aspire
+                                </div>
+                                <div className="landing-page-image">
 
+                                </div>
+                            </div>
             </header>
+            <div className="arrowbox">
+                <a href="#middle"> <i class="fa fa-angle-double-down"></i>
+                </a>
+            </div>
+
         </div>
     )
 }
